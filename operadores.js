43597a93b7f0b6b15aba6b454a38be82ae6d1b94
luissmga <luisrolfo1234@gmail.com>
2023-09-11 21:23:22 -0600

    04 operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+//operadores matemáticos
+// + - * /  **
+// += -= *= /= %=
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 / 10);
+console.log(10 * 10);
+console.log(10 % 10);
+console.log(10 ** 10); //potencia
+
+let num = 15;
+console.log(num += 5); //num = num + 5
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num /= 5);
+console.log(num %= 5);
+
+//operadores booleanos
+//> < >= <= != ===
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(1 !== 2);
+console.log(1 === 2);
+
+
+//operadores lógicos
+//and &&
+console.log(true && true);
+//or ||
+console.log(true || false);
+//not !
+console.log(!true);
